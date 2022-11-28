da532fb7a28694172d707e52c9f14b22cd53acd5
Арсений Анохин <artuhi.arsenii@gmail.com>
2022-11-28 06:44:49 +0300

    123

---
+++ KomunYslugi/Data/MongoExamples.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+
+namespace KomunYslugi.Data
+{
+    public class MongoExamples
+    {
+        public static void AddToDB(Customer user)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("KomUslugi");
+            var collection = database.GetCollection<Customer>("Customer");
+            collection.InsertOne(user);
+        }
+
+        public static void AddToDB(Designer user)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("KomUslugi");
+            var collection = database.GetCollection<Designer>("Designer");
+            collection.InsertOne(user);
+        }
+
+        public static void AddToDB(Developer user)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("KomUslugi");
+            var collection = database.GetCollection<Developer>("Developer");
+            collection.InsertOne(user);
+        }
+    }
+}
