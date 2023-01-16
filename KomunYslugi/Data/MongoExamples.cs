85adbd7fbeae461fc25a6d7e67dca527b444923c
Арсений Анохин <artuhi.arsenii@gmail.com>
2023-01-17 00:49:52 +0300

    add async

--- KomunYslugi/Data/MongoExamples.cs
+++ KomunYslugi/Data/MongoExamples.cs
@@ -47,6 +47,16 @@ namespace KomunYslugi.Data
             return one;
         }
 
+        public async static Task<User> FindAsync(string login)
+        {
+            await Task.Delay(3000);
+            var client = new MongoClient();
+            var database = client.GetDatabase("KomUslugi");
+            var collection = database.GetCollection<User>("User");
+            var one = collection.Find(x => x.Login == login).FirstOrDefault();
+            return one;
+        }
+
         public static void ReplaceByName(string login, User user)
         {
             var client = new MongoClient();
